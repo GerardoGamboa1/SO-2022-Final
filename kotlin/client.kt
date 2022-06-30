e4346dd956632977d2e9abc74e899ca35a1f03fd
GerardoGamboa1 <gerardoagm1902@gmail.com>
2022-06-30 17:16:32 -0300

    commit 30/06/2022

---
+++ kotlin/client.kt
@@ -0,0 +1,26 @@
+import java.net.Socket
+import kotlin.concurrent.thread
+
+fun main()
+{
+    val client = Socket("localhost", 5000)
+
+    val clientOutStream = client.getOutputStream()
+    val clientInStream = client.getInputStream()
+
+    thread {
+        while(true)
+        {
+            var nextByte = clientInStream.read()    // Loop que permite leer los datos ingresados al socket del cliente e imprimirlos en consola
+            print(nextByte.toChar())
+        }
+    }
+
+    thread {
+        while(true)
+        {
+            var input = readLine()!!                // Loop que permite escribir datos en el socket del servidor e imprimirlos en consola
+            clientOutStream.write(input.toByteArray())
+        }
+    }
+}
---
+++ progreso.md
@@ -0,0 +1,21 @@
+# Funcionando:
+
+1) Java to Java
+
+2) Kotlin to Kotlin
+
+3) Ruby to Ruby
+
+4) Server Java to Client Ruby
+
+5) Server Ruby to Client Java
+
+# En progreso:
+
+1) Server Java to Client Kotlin (Client no envía)
+
+2) Server Kotlin to Client Java (Server sólo envía al desconectarse)
+
+3) Server Kotlin to Client Ruby (Server sólo envía loop vacío)
+
+4) Server Ruby to Client Kotlin (Server sólo envía loop vacío)
